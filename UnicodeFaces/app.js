4cefc09bb3dbd41910037fb7cca365be6225e9c9
Trevor Adams <taadams1@dmacc.edu>
2019-11-07 22:27:38 -0600

    Initial Commit - A bit late

---
+++ UnicodeFaces/app.js
@@ -0,0 +1,46 @@
+﻿const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
+
+const { getIndex } = require('./routes/index');
+const port = 1337;
+
+//database configuration
+/*
+const db = mysql.createConnection({
+    host: '',
+    root: '',
+    password: '',
+    database: ''
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+*/
+
+//middleware configuration
+app.set('port', process.env.port || port);
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+//set up routes for app
+app.get('/', getIndex);
+
+/*
+app.get('/', (req, res) => {
+    res.send('Hello World.');
+});
+*/
+
+app.listen(port);
---
+++ UnicodeFaces/routes/index.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    getIndex: (req, res) => {
+        res.render('index.ejs', {
+            title: 'Unicode Faces',
+            author: 'Trevor Adams'
+        });
+                
+    }
+};
